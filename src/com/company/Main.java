45b260fbfaf77b71323c16f3c7921c1568dce565
jenniferali <jenniferoali@gmail.com>
2022-08-29 14:08:58 -0500

    Solar System ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         //write code here
+        solarSystem inception = new solarSystem();
+        inception.Sun = 1;
+        inception.Moon = 4;
+        inception.Planets = 2;
+
+        System.out.println("The basic structure is ready...☽");
 
     }
 }
